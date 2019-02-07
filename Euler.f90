9d089f856ee39a5050d8ad90da572d8811062dba
AM1GROUP2 <44943276+AM1GROUP2@users.noreply.github.com>
2019-02-07 08:55:27 +0100

    Delete Euler.f90

--- Euler.f90
+++
@@ -1,9 +0,0 @@
-subroutine Euler (F,t1,t2,U1,U2)
-    
-    procedure (ODE):: F
-    real,intent(in):: t1,t2,U1(:)
-    real,intent(out):: U2(:)
-    
-    U2 = U1 + (t2-t1) * F(U1,t1)
-    
-end subroutine
